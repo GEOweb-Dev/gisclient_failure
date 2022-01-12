0083ce7a8e1885448a81513de5704440e11821e9
anakin2212 <matteo.zero@taulogic.it>
2022-01-12 11:41:14 +0100

    Integrazione barriere nel nuovo algoritmo

--- public/services/iren/failureFunction.php
+++ public/services/iren/failureFunction.php
@@ -1,7 +1,7 @@
 <?php
 function costruisciGrafoRicorsivo($db, $selectedPipe, $dominio, $tipoField, $others, &$elements, $bVertex, $exclude) {
 	$res = array();
-	singoloArco($db, $selectedPipe, $dominio, $tipoField, $others, $res, explode(",",str_replace('\"',"",$exclude)), 1);
+	singoloArco($db, $selectedPipe, $dominio, $tipoField, $others, $res, explode(",",str_replace('\"',"",$exclude)), $bVertex, 1);
 	foreach($res as $row){
 		$elements["condotta"][] = array($row["tipo"],$row["id_elemento"]);
 		if($row["da_tipo"]!="altro" || in_array($row["da_nodo"], $bVertex))
@@ -11,7 +11,7 @@ function costruisciGrafoRicorsivo($db, $selectedPipe, $dominio, $tipoField, $oth
 	}
 }
 
-function singoloArco($db, $selectedPipe, $dominio, $tipoField, $others, &$rs, $bVertex, $depth) {
+function singoloArco($db, $selectedPipe, $dominio, $tipoField, $others, &$rs, $exclude, $include, $depth) {
 	if(!empty($selectedPipe) && count(array_filter($rs, function($single) use($selectedPipe){
 		return $single['id_arco']==$selectedPipe;
 	}))==0) {
@@ -22,20 +22,21 @@ function singoloArco($db, $selectedPipe, $dominio, $tipoField, $others, &$rs, $b
 		$stmt->execute();
 		while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$rs[] = $row;
-			singoloNodo($db, $row['da_tipo'], $row['da_nodo'], $dominio, $tipoField, $others, $rs, $bVertex, $depth);	
-			singoloNodo($db, $row['a_tipo'], $row['a_nodo'], $dominio, $tipoField, $others, $rs, $bVertex, $depth);
+			singoloNodo($db, $row['da_tipo'], $row['da_nodo'], $dominio, $tipoField, $others, $rs, $exclude, $include, $depth);	
+			singoloNodo($db, $row['a_tipo'], $row['a_nodo'], $dominio, $tipoField, $others, $rs, $exclude, $include, $depth);
 		}
 	}
 }
 
-function singoloNodo($db,$tipo, $nodo, $dominio, $tipoField, $others, &$rs, $bVertex, $depth){
+function singoloNodo($db,$tipo, $nodo, $dominio, $tipoField, $others, &$rs, $exclude, $include, $depth){
 	//if(in_array($tipo, $others) || in_array($nodo, $bVertex)) {
 	$st1 = $db->prepare("select tipo_nodo, id_elemento, arco_entrante, arco_uscente from grafo.nodi_$dominio where id_nodo=".$nodo);
 	$st1->execute();
 	while($row = $st1->fetch(PDO::FETCH_ASSOC)) {
-		if(in_array($row['tipo_nodo'],$others) || in_array($row['id_elemento'],$bVertex)) {
+		error_log($row['id_elemento']." ".json_encode($include));
+		if((in_array($row['tipo_nodo'],$others) || in_array($row['id_elemento'],$exclude)) && !in_array($nodo, $include)){
 			foreach(array_merge(explode(",",str_replace(array('{','}'),"",$row['arco_entrante'])), explode(",", str_replace(array('{','}'),"",$row['arco_uscente']))) as $bow)
-				singoloArco($db, $bow, $dominio, $tipoField, $others, $rs, $bVertex, $depth+1);
+				singoloArco($db, $bow, $dominio, $tipoField, $others, $rs, $exclude, $include, $depth+1);
 		}
 	}
 }
